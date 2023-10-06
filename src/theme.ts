6cc2e886e9016a63bc6ecf139e70b5450aafe683
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-06 08:24:58 +0200

    created theme file with basic colors, breakpoints

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+  color: {
+    primary: "rgba(215, 155, 28)",
+    primaryTransparent: "rgba(215, 155, 28, 0.7)",
+    secondary: "rgba(239, 230, 212, 0.8)",
+    light: "rgba(235, 232, 226)",
+    dark: "rgba(0, 18, 36)",
+    darkTransparent: "rgba(0, 18, 36, 0.5)",
+  },
+  breakpoint: {
+    mobile: 767,
+  },
+};
+
+export default theme;
