95e3694d0d8678ab4acbaf7ea15cfceacddc4fec
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-06 08:37:29 +0200

    created reusable Layout for different screens

---
+++ src/common/Layout/index.tsx
@@ -0,0 +1,21 @@
+import Footer from "./Footer";
+import { Container, Main, Header, Title } from "./styled";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  backgroundImage: string;
+}
+
+const Layout = ({ children, backgroundImage }: LayoutProps) => {
+  return (
+    <Container backgroundImage={backgroundImage}>
+      <Header>
+        <Title>BattleShip: Random Routes</Title>
+      </Header>
+      <Main>{children}</Main>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Layout;
