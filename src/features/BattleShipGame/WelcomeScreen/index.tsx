d7721239c0b3f4ca20a15c62f851b986338c9360
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-07 09:12:51 +0200

    created WelcomeScreen with functionalities

---
+++ src/features/BattleShipGame/WelcomeScreen/index.tsx
@@ -0,0 +1,25 @@
+import Layout from "../../../common/Layout";
+import Button from "../../../common/Button";
+import { GameProps } from "../useGame";
+import backgroundWelcome from "./backgroundWelcome.jpg";
+import Rules from "./Rules";
+import useToggleRules from "./useToggleRules";
+import { WelcomeText } from "./styled";
+
+const WelcomeScreen = ({ handleStartGame }: GameProps) => {
+  const { openRules, handleToggleRules } = useToggleRules();
+
+  return (
+    <Layout backgroundImage={backgroundWelcome}>
+      <WelcomeText>
+        Are you ready to lead your fleet and face off against your opponent on
+        uncharted waters?
+      </WelcomeText>
+      <Button textBtn="Start Game" onClick={handleStartGame} />
+      <Button textBtn="Game Rules" onClick={handleToggleRules} />
+      {openRules && <Rules handleToggleRules={handleToggleRules} />}
+    </Layout>
+  );
+};
+
+export default WelcomeScreen;
