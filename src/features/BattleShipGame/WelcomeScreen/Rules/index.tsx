4d9d208b09ae6e54ba9f04005db462f8816f12a4
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-08 11:41:40 +0200

    created basic Rules component

---
+++ src/features/BattleShipGame/WelcomeScreen/Rules/index.tsx
@@ -0,0 +1,41 @@
+import Button from "../../../../common/Button";
+import { RulesProps } from "../useToggleRules";
+import { StyledRules } from "./styled";
+
+const Rules = ({ handleToggleRules }: RulesProps) => {
+  return (
+    <StyledRules>
+      <p>
+        "BattleShip: Random Routes" is a modern take on the classic Battleship
+        game, offering dynamic gameplay for two players. It utilizes randomly
+        generated boards, providing endless variety and challenges during sea
+        battles.
+      </p>
+      <h3>Key Features of the Game:</h3>
+      <ul>
+        <li>
+          <span>Randomly Generated Boards:</span>
+          Each game starts with randomly generated boards on which both players
+          place their fleets. This means that each battle is unique, and players
+          must rely on their tactical intelligence.
+        </li>
+
+        <li>
+          <span>Fleets and Tactics:</span>
+          Players command fleets consisting of various types of ships. Each ship
+          occupies a different number of spaces on the board, introducing a
+          strategic element.
+        </li>
+        <li>
+          <span>Competition and Precision:</span>
+          The game fosters competition between players, requiring precise shots
+          and tactical decisions. The goal is to sink all the opponent's ships
+          before they do the same.
+        </li>
+      </ul>
+      <Button textBtn="Close" onClick={handleToggleRules} />
+    </StyledRules>
+  );
+};
+
+export default Rules;
