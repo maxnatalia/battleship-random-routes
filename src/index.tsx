dff008a536b14f8a30707c469ab62197e5a2ff33
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-08 11:44:02 +0200

    added ThemeProvider with theme in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { ThemeProvider } from "styled-components";
 import reportWebVitals from "./reportWebVitals";
-import { GlobalStyle } from "./GlobalStyle";
+import { GlobalStyle } from "./core/GlobalStyle";
+import theme from "./core/theme";
+import App from "./core/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
