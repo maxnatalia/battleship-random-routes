a3fe6cf424c339f987406d4aba384a5124d0d4d1
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-11 16:43:42 +0200

    created common Icons component with react icons

---
+++ src/common/Icons/styled.ts
@@ -0,0 +1,23 @@
+import styled, { DefaultTheme } from "styled-components";
+import {
+  GiSubmarineMissile,
+  GiShoonerSailboat,
+  GiAnchor,
+} from "react-icons/gi";
+
+const commonStyles = {
+  fontSize: "30px",
+  color: `${({ theme }: DefaultTheme) => theme.color.primary}`,
+};
+
+export const IconSunkShip = styled(GiAnchor)`
+  ${commonStyles};
+`;
+
+export const IconMiss = styled(GiSubmarineMissile)`
+  ${commonStyles};
+`;
+
+export const IconHitShip = styled(GiShoonerSailboat)`
+  ${commonStyles};
+`;
