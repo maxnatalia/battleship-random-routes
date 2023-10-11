9c36227f5af1b217ddabdade0cfe62c564fe5e10
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-11 16:45:50 +0200

    added basic styles for Consoles component

---
+++ src/features/BattleShipGame/GameScreen/Consoles/styled.ts
@@ -0,0 +1,47 @@
+import styled, { css } from "styled-components";
+
+interface ActivePlayerProps {
+  $activePlayer?: boolean;
+}
+
+export const StyledConsoles = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+`;
+
+export const Console = styled.div<ActivePlayerProps>`
+  padding: 20px;
+  border: 6px solid ${({ theme }) => theme.color.darkTransparent};
+  border-radius: 20px;
+
+  ${({ $activePlayer }) =>
+    $activePlayer &&
+    css`
+      border: 6px solid ${({ theme }) => theme.color.primary};
+      font-size: 22px;
+    `}
+`;
+
+export const Progress = styled.div`
+  padding: 20px;
+  background-color: ${({ theme }) => theme.color.darkTransparent};
+  border-radius: 20px;
+`;
+
+export const LineProgress = styled.div`
+  line-height: 2;
+`;
+
+export const EnemyInfo = styled.div`
+  margin: 20px auto;
+  padding: 10px;
+  border-radius: 20px;
+  background-image: linear-gradient(
+    rgba(215, 155, 28, 0.4),
+    rgba(215, 155, 28, 0.6)
+  );
+  font-size: 18px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.dark};
+`;
