b796d6accfc7679040d7e7a1e10e45f71061f8bc
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-11 16:28:12 +0200

    added styling for Section common component

---
+++ src/common/Section/styled.ts
@@ -0,0 +1,37 @@
+import styled, { css } from "styled-components";
+import { SectionProps } from ".";
+
+export const StyledSection = styled.div<SectionProps>`
+  padding: 20px;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+
+  ${({ $top }) =>
+    $top &&
+    css`
+      flex-direction: column;
+    `}
+
+  ${({ $center }) =>
+    $center &&
+    css`
+      justify-content: center;
+    `}
+
+    ${({ $modal }) =>
+    $modal &&
+    css`
+      width: 60vw;
+      border-radius: 20px;
+      background-color: ${({ theme }) => theme.color.primary};
+      color: ${({ theme }) => theme.color.dark};
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 10;
+    `}
+`;
