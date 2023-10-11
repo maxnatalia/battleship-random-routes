351dd64182566b3baa6e95cbb310f4c176560c1e
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-11 16:28:51 +0200

    updated - added props for  main components

--- src/features/BattleShipGame/index.tsx
+++ src/features/BattleShipGame/index.tsx
@@ -3,11 +3,29 @@ import GameScreen from "./GameScreen";
 import WelcomeScreen from "./WelcomeScreen";
 
 const BattleShipGame = () => {
-  const { startGame, handleStartGame, handleExitGame } = useGame();
+  const {
+    startGame,
+    handleStartGame,
+    handleExitGame,
+    playerBoard1,
+    playerBoard2,
+    handleClickCell,
+    isCurrentPlayer1,
+    scores,
+    message,
+  } = useGame();
   return (
     <>
       {startGame ? (
-        <GameScreen handleExitGame={handleExitGame} />
+        <GameScreen
+          handleExitGame={handleExitGame}
+          playerBoard1={playerBoard1}
+          playerBoard2={playerBoard2}
+          handleClickCell={handleClickCell}
+          isCurrentPlayer1={isCurrentPlayer1}
+          scores={scores}
+          message={message}
+        />
       ) : (
         <WelcomeScreen handleStartGame={handleStartGame} />
       )}
