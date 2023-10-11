c9212d444eee37634ad5a86b56a2a6489eae02aa
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-11 16:42:55 +0200

    updated - added animation and improved props style

--- src/features/BattleShipGame/GameScreen/Board/styled.ts
+++ src/features/BattleShipGame/GameScreen/Board/styled.ts
@@ -1,29 +1,40 @@
-import styled, { css } from "styled-components";
-
+import styled, { css, keyframes } from "styled-components";
 interface BoardPlayerProps {
-  activePlayer: boolean;
+  $activePlayer: boolean;
 }
 
 interface CellProps {
-  isClicked: boolean;
-  isShipClicked: boolean;
-  isShipSunk: boolean;
+  $isClicked: boolean;
+  $isShipClicked: boolean;
+  $isShipSunk: boolean;
 }
 
+const swingAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(10deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+`;
+
 export const BoardPlayer = styled.div<BoardPlayerProps>`
-  border: 2px solid orange;
+  border: 6px solid ${({ theme }) => theme.color.primaryTransparent};
+  border-radius: 20px;
   padding: 10px;
   transition: all 0.6s ease-in-out;
 
-  ${({ activePlayer }) =>
-    !activePlayer &&
+  ${({ $activePlayer }) =>
+    !$activePlayer &&
     css`
       transform: scale(0.5);
       pointer-events: none;
       filter: blur(1px);
     `}
 `;
-export const Grid = styled.div``;
 
 export const Row = styled.div`
   display: flex;
@@ -33,35 +44,43 @@ export const Row = styled.div`
 export const Cell = styled.div<CellProps>`
   width: 50px;
   height: 50px;
-  border: 1px solid black;
+  border: 1px solid transparent;
   display: flex;
   align-items: center;
   justify-content: center;
-  cursor: pointer;
+  cursor: ${({ $isClicked, $isShipClicked, $isShipSunk }) =>
+    $isClicked || $isShipClicked || $isShipSunk ? "not-allowed" : "pointer"};
+  transition: all 0.5s ease-in-out;
 
   &:hover {
-    background-color: ${({ theme }) => theme.color.primaryTransparent};
-    color: ${({ theme }) => theme.color.dark};
+    ${({ $isClicked, $isShipClicked, $isShipSunk, theme }) =>
+      !($isClicked || $isShipClicked || $isShipSunk) &&
+      css`
+        background-color: ${theme.color.primaryTransparent};
+        color: ${theme.color.dark};
+        font-weight: 600;
+      `}
   }
 
-  ${({ isClicked }) =>
-    isClicked &&
+  ${({ $isClicked }) =>
+    $isClicked &&
     css`
-      background-color: blue;
-      cursor: not-allowed;
+      border: 1px solid ${({ theme }) => theme.color.primaryTransparent};
     `}
 
-  ${({ isShipClicked }) =>
-    isShipClicked &&
+  ${({ $isShipClicked, $isShipSunk }) =>
+    $isShipClicked &&
+    !$isShipSunk &&
     css`
-      background-color: red;
-      cursor: not-allowed;
+      animation: ${swingAnimation} 0.5s ease infinite;
     `}
 
-    ${({ isShipSunk }) =>
-    isShipSunk &&
+    ${({ $isShipSunk }) =>
+    $isShipSunk &&
     css`
-      background-color: goldenrod;
-      cursor: not-allowed;
+      background-image: linear-gradient(
+        rgba(0, 18, 36, 0.4),
+        rgba(215, 155, 28, 0.5)
+      );
     `}
 `;
