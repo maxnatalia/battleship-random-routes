8c347d6747ce0b260cfd4a2e9d2c728276a4615e
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-11 16:42:24 +0200

    updated - implemented props and Icons

--- src/features/BattleShipGame/GameScreen/Board/index.tsx
+++ src/features/BattleShipGame/GameScreen/Board/index.tsx
@@ -1,31 +1,45 @@
-import { BoardItemProps } from "../../useGame";
-import { BoardPlayer, Cell, Grid, Row } from "./styled";
+import {
+  IconHitShip,
+  IconMiss,
+  IconSunkShip,
+} from "../../../../common/Icons/styled";
+import { BoardItemProps } from "../../interfaces";
+import { BoardPlayer, Cell, Row } from "./styled";
 
 interface BoardProps {
   playerBoard: BoardItemProps[][];
-  handleClickCell: (item: BoardItemProps) => void;
-  activePlayer: boolean;
+  handleClickCell: (item: BoardItemProps, currentPlayer: "P1" | "P2") => void;
+  $activePlayer: boolean;
+  currentPlayer: "P1" | "P2";
 }
 
-const Board = ({ handleClickCell, playerBoard, activePlayer }: BoardProps) => {
+const Board = ({
+  handleClickCell,
+  playerBoard,
+  $activePlayer,
+  currentPlayer,
+}: BoardProps) => {
   return (
-    <BoardPlayer activePlayer={activePlayer}>
+    <BoardPlayer $activePlayer={$activePlayer}>
       {playerBoard.map((row, rowIndex) => (
-        <Grid key={rowIndex}>
+        <div key={rowIndex}>
           <Row>
             {row.map(col => (
               <Cell
                 key={col.id}
-                onClick={() => handleClickCell(col)}
-                isClicked={col.isClicked}
-                isShipClicked={col.isShip && col.isClicked}
-                isShipSunk={col.isSunk}
+                onClick={() => handleClickCell(col, currentPlayer)}
+                $isClicked={col.isClicked}
+                $isShipClicked={col.isHit}
+                $isShipSunk={col.isSunk}
               >
-                {col.isShip ? "Y" : `${col.label}`}
+                {col.isClicked && <IconMiss />}
+                {col.isHit && !col.isSunk && <IconHitShip />}
+                {col.isSunk && <IconSunkShip />}
+                {!col.isClicked && !col.isHit && !col.isSunk && col.label}
               </Cell>
             ))}
           </Row>
-        </Grid>
+        </div>
       ))}
     </BoardPlayer>
   );
