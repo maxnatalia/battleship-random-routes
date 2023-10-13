c0995cadc583674addcbb01abcdb3562c91a17d6
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-13 06:27:06 +0200

    added breakpoints in styles for WelcomeScreen

--- src/features/BattleShipGame/WelcomeScreen/styled.ts
+++ src/features/BattleShipGame/WelcomeScreen/styled.ts
@@ -7,4 +7,8 @@ export const WelcomeText = styled.h2`
   font-weight: 600;
   text-align: center;
   line-height: 1.6;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 18px;
+  }
 `;
