1909517babc05a9d1aeb030cd2387e26d1def99b
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-13 06:25:16 +0200

    updated - added breakpoints and deleted color

--- src/core/theme.ts
+++ src/core/theme.ts
@@ -3,12 +3,12 @@ const theme = {
     primary: "rgba(215, 155, 28)",
     primaryTransparent: "rgba(215, 155, 28, 0.7)",
     secondary: "rgba(239, 230, 212, 0.8)",
-    light: "rgba(235, 232, 226)",
     dark: "rgba(0, 18, 36)",
     darkTransparent: "rgba(0, 18, 36, 0.5)",
   },
   breakpoint: {
-    mobile: 767,
+    mobile: 768,
+    tablet: 1024,
   },
 };
 
